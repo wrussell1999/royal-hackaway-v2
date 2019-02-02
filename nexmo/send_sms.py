f9b4b2437ea03a4ebf8f594d579f83dcd93d694b
wrussell1999 <jetpowerss@gmail.com>
2019-02-02 18:25:53 +0000

    Send SMS and receive calls

---
+++ nexmo/send_sms.py
@@ -0,0 +1,8 @@
+import nexmo
+import json
+
+with open('config.json', 'r') as config_file:
+    config = config_file.read()
+
+client = nexmo.Client(key=config['APPLICATION_ID'], secret=config['SECRET'])
+print(client.send_message({'from': 'Royal Hackaway', 'to': config['TO_NUMBER'], 'text': 'Test message'}))
--- nexmo/voice.py
+++ nexmo/voice.py
@@ -1,14 +1,17 @@
 import nexmo
 from pprint import pprint
-import json
 
-with open("config.json") as file:
-    config = json.load(file)
+with open('config.json', 'r') as config_file:
+    config = config_file.read()
+
+with open('private.key', 'r') as content_file:
+    content = content_file.read()
 
 client = nexmo.Client(
     application_id=config['APPLICATION_ID'],
-    private_key=config['APPLICATION_PRIVATE_KEY_PATH'],
+    private_key=content,
 )
+
 
 response = client.create_call({
   'to': [{'type': 'phone', 'number': config['TO_NUMBER']}],
@@ -16,4 +19,4 @@ response = client.create_call({
   'answer_url': ['https://developer.nexmo.com/ncco/tts.json']
 })
 
-pprint(response)
+pprint(response_text)
