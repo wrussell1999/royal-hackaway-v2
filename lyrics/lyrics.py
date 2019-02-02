82bb8dd9d99ce8dfa036bd3465d84c41e14a0e46
Justin Chadwell <jedevc@gmail.com>
2019-02-02 15:26:11 +0000

    Add lyrics parser

--- lyrics/lyrics.py
+++ lyrics/lyrics.py
@@ -1,2 +1,19 @@
+import re
+
+from pprint import pprint
+
 def main():
-    print("Hello world")
+    with open('lyrics/rap_god.txt') as f:
+        rap_god = f.read()
+
+    pprint(parse_lyrics(rap_god))
+
+def parse_lyrics(lyrics, clean = True):
+    if clean:
+        lyrics = re.sub('\[.*\]', '', lyrics)
+        lyrics = lyrics.strip()
+
+    verses = re.split('\n\n+', lyrics)
+    verses = [line.split('\n') for line in verses]
+
+    return verses
