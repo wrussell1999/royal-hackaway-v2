42d05c725f2b8d84e7916580bc805b686fb852ba
Justin Chadwell <jedevc@gmail.com>
2019-02-02 16:17:21 +0000

    Move context stuff into different file

---
+++ lyrics/context.py
@@ -0,0 +1,32 @@
+import nltk
+
+class Context:
+    def __init__(self, contents):
+        self._words = nltk.word_tokenize(contents)
+        self._tagged_words = nltk.pos_tag(self._words)
+
+    def generate(self):
+        return ' '.join(self._words)
+
+    def get(self, position):
+        return self._tagged_words[position]
+
+    def set(self, position, value):
+        self._words[position] = value
+        self._tagged_words = nltk.pos_tag(self._words)
+
+    def nouns(self):
+        for i, (word, tag) in enumerate(self._tagged_words):
+            if tag[0] == 'N':
+                yield Token(self, i)
+
+class Token:
+    def __init__(self, context, position):
+        self._context = context
+        self._position = position
+
+    def get(self):
+        return self._context.get(self._position)
+
+    def set(self, value):
+        return self._context.set(self._position, value)
--- lyrics/lyrics.py
+++ lyrics/lyrics.py
@@ -1,10 +1,11 @@
 import re
-import nltk
 
 import random
 
 from pprint import pprint
 import code
+
+from .context import Context
 
 def main():
     lyrics = generate_lyrics('cat')
@@ -37,34 +38,3 @@ def parse_lyrics(lyrics, clean = True):
     verses = [line.split('\n') for line in verses]
 
     return verses
-
-class Context:
-    def __init__(self, contents):
-        self._words = nltk.word_tokenize(contents)
-        self._tagged_words = nltk.pos_tag(self._words)
-
-    def generate(self):
-        return ' '.join(self._words)
-
-    def get(self, position):
-        return self._tagged_words[position]
-
-    def set(self, position, value):
-        self._words[position] = value
-        self._tagged_words = nltk.pos_tag(self._words)
-
-    def nouns(self):
-        for i, (word, tag) in enumerate(self._tagged_words):
-            if tag[0] == 'N':
-                yield Token(self, i)
-
-class Token:
-    def __init__(self, context, position):
-        self._context = context
-        self._position = position
-
-    def get(self):
-        return self._context.get(self._position)
-
-    def set(self, value):
-        return self._context.set(self._position, value)
