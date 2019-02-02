ec561b17bd95a622165cde39ab1dd5fcbafaf96a
Justin Chadwell <jedevc@gmail.com>
2019-02-02 21:55:24 +0000

    Change config stuff

--- lyrics/lyrics.py
+++ lyrics/lyrics.py
@@ -5,37 +5,41 @@ import tswift
 
 from .generator import Generator
 
+CONFIG = 'config.json'
+
+CACHE = 'cache/'
+SONG_CACHE = 'cache/songs.json'
+
 def main():
-    ARTISTS = read_from_config('artists.json')
+    with open(CONFIG) as f:
+        config = json.load(f)
 
-    SONGS = read_from_config('cache/songs.json')
-    if SONGS is None:
-        os.makedirs('cache/', exist_ok = True)
+    if not os.path.exists(SONG_CACHE) or is_more_recent(CONFIG, SONG_CACHE):
+        os.makedirs(CACHE, exist_ok = True)
 
-        SONGS = []
-        for artist, songs in ARTISTS.items():
+        cache = []
+        for artist, songs in config.items():
             if isinstance(songs, str):
                 if songs == '*':
                     # all songs by artist
-                    SONGS.extend((song.title, artist) for song in tswift.Artist(artist).songs)
+                    cache.extend((song.title, artist) for song in tswift.Artist(artist).songs)
                 else:
                     # only one song by artist
-                    SONGS.append((songs, artist))
+                    cache.append((songs, artist))
             else:
                 # multiple songs by artist
-                SONGS.extend((song, artist) for song in songs)
+                cache.extend((song, artist) for song in songs)
 
-        with open('cache/songs.json', 'w') as f:
-            json.dump(SONGS, f, indent=4)
+        with open(SONG_CACHE, 'w') as f:
+            json.dump(cache, f, indent=4)
+    else:
+        with open(SONG_CACHE) as f:
+            cache = json.load(f)
 
-    gen = Generator(SONGS)
+    gen = Generator(cache)
 
     lyrics = gen.generate_lyrics('cat')
     print(lyrics)
 
-def read_from_config(filename):
-    try:
-        with open(filename) as f:
-            return json.load(f)
-    except FileNotFoundError:
-        return None
+def is_more_recent(fp1, fp2):
+    return os.path.getmtime(fp1) > os.path.getmtime(fp2)
