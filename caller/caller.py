60b976d261e0484f5bf1fcc7352e3376ac2803f9
Justin Chadwell <jedevc@gmail.com>
2019-02-03 06:38:13 +0000

    Stream an mp3

--- caller/caller.py
+++ caller/caller.py
@@ -5,12 +5,11 @@ from pprint import pprint
 import flask
 from flask import Flask, request, jsonify
 
+from . import config
 from . import voice
 
 def main():
     app = Flask(__name__)
-
-    themes = {}
 
     @app.route('/webhooks/inbound_sms', methods=['GET', 'POST'])
     def inbound_sms():
@@ -19,21 +18,43 @@ def main():
         else:
             data = dict(request.form) or dict(request.args)
             mobile_number = data['msisdn']
-            themes[mobile_number] = data['text'].split(' ')[-1]
+            theme = data['text'].split(' ')[-1]
+
             print('User number: ' + mobile_number)
-            print('Theme: ' + themes[mobile_number])
+            print('Theme: ' + theme)
+
+            # TODO: generate recording here in recordings/{number}.mp3
+
             voice.make_call(mobile_number)
 
         return ('', 204)
 
     @app.route('/calls/<number>.json')
     def send(number):
-        return flask.jsonify([
-            {
-                "action": "talk",
-                "voiceName": "Brian",
-                "text": themes[number]
-            }
-        ])
+        server = config.config['SERVER']
+        recording = f'{server}/recordings/{number}.mp3'
+
+        if True:
+        # if os.path.exists(f'recordings/{number}.mp3')
+            return flask.jsonify([
+                {
+                    'action': 'stream',
+                    'streamUrl': [recording]
+                }
+            ])
+        else:
+            return flask.jsonify([
+                {
+                    'action': 'talk',
+                    'text': 'Error: could not find recording'
+                }
+            ])
+
+    @app.route('/recordings/<path:filename>')
+    def get_recording(filename):
+        path = os.path.abspath('output.mp3')
+        return flask.send_file(path)
+        # path = os.path.abspath('cache/recordings/')
+        # return flask.send_from_directory(path, filename)
 
     app.run(port=3000)
