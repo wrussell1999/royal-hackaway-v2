afd1a201658ecb24b24b9fdf3f2b319cb4581b45
wrussell1999 <jetpowerss@gmail.com>
2019-02-03 07:58:21 +0000

    MP3 method added

--- tts/text_to_mp3.py
+++ tts/text_to_mp3.py
@@ -2,24 +2,21 @@ from google.cloud import texttospeech
 
 client = texttospeech.TextToSpeechClient()
 
-test_data = '''I'm beginning to feel like a Rap God, Rap God
-All my people from the front to the back nod, back nod
-Now who thinks their arms are long enough to slap box, slap box?
-They said I rap like a robot, so call me Rapbot'''
-synthesis_input = texttospeech.types.SynthesisInput(text=test_data)
+def make_mp3(text, filename):
+    synthesis_input = texttospeech.types.SynthesisInput(text=text)
 
-# Build the voice request, select the language code ("en-US") and the ssml
-# voice gender ("neutral")
-voice = texttospeech.types.VoiceSelectionParams(
-    language_code='en-US',
-    ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
+    # Build the voice request, select the language code ("en-US") and the ssml
+    # voice gender ("neutral")
+    voice = texttospeech.types.VoiceSelectionParams(
+        language_code='en-US',
+        ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
 
-audio_config = texttospeech.types.AudioConfig(
-    audio_encoding=texttospeech.enums.AudioEncoding.MP3,
-    speaking_rate=0.70,
-    pitch=-15.0)
+    audio_config = texttospeech.types.AudioConfig(
+        audio_encoding=texttospeech.enums.AudioEncoding.MP3,
+        speaking_rate=0.70,
+        pitch=-15.0)
 
-response = client.synthesize_speech(synthesis_input, voice, audio_config)
-with open('output.mp3', 'wb') as out:
-    out.write(response.audio_content)
-    print('Audio content written to file "output.mp3"')
+    response = client.synthesize_speech(synthesis_input, voice, audio_config)
+    with open(filename, 'wb') as out:
+        out.write(response.audio_content)
+        print('Audio content written to file "output.mp3"')
