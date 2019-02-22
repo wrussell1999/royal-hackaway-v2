d24cf9607a32e99ffb9bf9b3fde024091da01034
Will Russell <jetpowerss@gmail.com>
2019-02-22 00:05:27 +0000

    Update text_to_mp3.py

--- caller/text_to_mp3.py
+++ caller/text_to_mp3.py
@@ -8,8 +8,6 @@ def make_mp3(text, filename):
     print("Starting sound synthesis...")
     synthesis_input = texttospeech.types.SynthesisInput(text=text)
 
-    # Build the voice request, select the language code ("en-US") and the ssml
-    # voice gender ("neutral")
     voice = texttospeech.types.VoiceSelectionParams(
         language_code='en-US',
         ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
@@ -26,11 +24,8 @@ def make_mp3(text, filename):
         print(f'Audio content written to {filename}')
     sound1 = AudioSegment.from_mp3(filename)
     sound2 = AudioSegment.from_mp3("supreme.mp3")
-
-    # mix sound2 with sound1, starting at 5000ms into sound1)
     output = sound1.overlay(sound2, position=100)
 
-    # save the result
     new_filename = os.path.join(os.path.dirname(filename), 'mod-' + os.path.basename(filename))
     print(new_filename)
     output.export(new_filename, format="mp3")
